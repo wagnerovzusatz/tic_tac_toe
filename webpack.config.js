a8941cb30d81486e82bebaba6718d1c6f9f8c6fe
Dennis Treder <dt@neopoly.de>
2015-06-12 10:25:20 +0200

    Add fileloader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.css$/, loader: "style!css" }
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.html/, loader: 'file?name=[name].[ext]' }
     ]
   }
 };
